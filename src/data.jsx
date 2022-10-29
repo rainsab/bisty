def8da044011bf8a36de43bb14a7cba1eb921c01
Martin Bašniar <m.basniar@gmail.com>
2022-10-29 17:38:44 +0200

    started off this project

---
+++ src/data.jsx
@@ -0,0 +1,67 @@
+export default [
+    {
+        id: 1,
+        band: "Lamp of Murmuur",
+        bandcamp: "https://lampofmurmuur.bandcamp.com/",
+        genre: "black metal",
+        date: "28th of May",
+        location: "Klub 007 Strahov",
+        img: ""
+    },
+    {
+        id: 2,
+        band: "Dödsrit",
+        bandcamp: "https://dodsrit.bandcamp.com/",
+        genre: "black metal / crust",
+        date: "28th of May",
+        location: "Klub 007 Strahov",
+    },
+    {
+        id: 3,
+        band: "Gatecreeper",
+        bandcamp: "https://gatecreeper.bandcamp.com/",
+        genre: "death metal/ hardcore",
+        date: "24th of June",
+        location: "Underdogs Prague",
+    },
+    {
+        id: 4,
+        band: "Vole",
+        bandcamp: "https://softskins.bandcamp.com/",
+        genre: "punk / hardcore",
+        date: "24th of June",
+        location: "Underdogs Prague",
+    },
+    {
+        id: 5,
+        band: "Dawn Ray'd",
+        bandcamp: "https://dawnrayd.bandcamp.com/",
+        genre: "black metal",
+        date: "29th of July",
+        location: "Fluff Fest 2022",
+    },
+    {
+        id: 6,
+        band: "Autor",
+        bandcamp: "https://autor.bandcamp.com/",
+        genre: "post-punk",
+        date: "29th of July",
+        location: "Fluff Fest 2022",
+    },
+    {
+        id: 7,
+        band: "Portrayal of Guilt",
+        bandcamp: "https://portrayalofguilt.bandcamp.com/",
+        genre: "black metal / hardcore",
+        date: "29th of July",
+        location: "Fluff Fest 2022",
+    },
+    {
+        id: 8,
+        band: "Iceage",
+        bandcamp: "https://iceage.bandcamp.com/",
+        genre: "punk / rock",
+        date: "29th of July",
+        location: "Fluff Fest 2022",
+    }
+]
