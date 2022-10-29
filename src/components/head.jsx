08f9cc3b899bf116fc4512236cf3707f6b544e02
Martin Bašniar <m.basniar@gmail.com>
2022-10-29 18:36:09 +0200

    updated header text

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Head() {
     return (
         <section className="head">
-            <h1>bands I've seen this year</h1>
+            <h1>bands I've seen live this year</h1>
         </section>
     )
 }
