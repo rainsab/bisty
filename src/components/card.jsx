0a66b637b123ad43190d02a62c748038eed56bf7
Martin Bašniar <m.basniar@gmail.com>
2022-10-31 11:40:12 +0100

    fixed bug with images on mobile screens

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Card(props) {
     return (
         <div className="card">
-            <img src={`../images/${props.img}`} alt="" className="card-img"/>
+            <img src={`../images/${props.img}`} alt="" className="card-img" width="710" height="420" />
             <div className="card-text">
                 <h2>{props.band}</h2>
                 <p>{props.genre}</p>
